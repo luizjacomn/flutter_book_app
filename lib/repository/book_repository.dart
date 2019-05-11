a8cef7789dc38928e0ae9855edfec2a492980202
Luiz Jacó <luizjacomn@gmail.com>
2019-05-11 19:49:49 -0300

    Configurando busca na api do Google Books

--- lib/repository/book_repository.dart
+++ lib/repository/book_repository.dart
@@ -1,9 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter_book_app/api_response.dart';
 import 'package:flutter_book_app/bloc/state/home_page_state.dart';
 import 'package:http/http.dart' as http;
 
 class BookRepository {
-  Future<HomePageState> getBooks(String query) {
-    return Future.delayed(Duration(seconds: 2),
-        () => HomePageStateError(message: 'Falha de conexão!'));
+  final String baseUrl = 'https://www.googleapis.com/books/v1/volumes';
+
+  Future<HomePageState> getBooks(String query) async {
+    var response = await http.get('$baseUrl?q=$query');
+
+    if (response.statusCode == 200) {
+      var body = response.body;
+      var data = ApiResponse.fromJson(json.decode(body));
+
+      return HomePageStateSuccess(books: data.items);
+    }
+
+    return HomePageStateError(
+      message: 'Houve uma falha de conexão!',
+    );
   }
 }
+
+//var response = http.get('$baseUrl?q=$query');
+//
+//response.then((res) {
+//var body = res.body;
+//var data = ApiResponse.fromJson(json.decode(body));
+//return HomePageStateSuccess(books: data.items);
+//}).catchError((e) {
+//return HomePageStateError(message: e);
+//});
+//return null;
